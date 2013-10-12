dafc970a6b556e4bc1e311de9bc39e2f8478489e
walido <walidowais@gmail.com>
2013-10-12 02:47:16 -0500

    facts portion works, questions is #todo, added test files computers.fax and modified version of it test.fax

--- facts.c
+++ facts.c
@@ -1,13 +1,20 @@
+// Walid Owais
+// EID: wo783
+// Section #: 53845
+
 #include <stdio.h>
 #include <stdlib.h>
 
 void process(void);
-void add_node(char c);
+int add_node(char *p_name, char *p_prop, char *p_val);
 void print_list(void);
 void liberate_list(void);
+char trim_white(char c, FILE *input);
 
 struct node{
-	char data;
+	char *name;
+	char *property;
+	char *value;
 	struct node *next;
 };
 
@@ -15,28 +22,43 @@ FILE *facts = NULL;
 FILE *questions = NULL;
 struct node *root;
 struct node *last;
-int size;
+int lst_size;
 
 int main(int argc, char const *argv[])
 {
 	if(argc < 2){
 		facts = stdin;
 		process();
-		// fprintf(stderr, "no facts file specified\n");
-		// fprintf(stderr, "usage: facts fact_file.fax [question_file.q]\n");
-		// exit(1);
 	}
 	else if(argc == 2){
 		facts = fopen(argv[1], "r");
+		if(facts == NULL){
+			fprintf(stderr, "Error opening facts file: %s\n", argv[1]);
+			exit(1);
+		}
+		
 		questions = stdin;
-	}
-	else if(argc == 3){
-		facts = fopen(argv[1], "r");
-		questions = fopen(argv[2], "r");
+		
+		process();
 	}
 	else{
-		fprintf(stderr, "too many arguments specified\n");
-		fprintf(stderr, "usage: facts fact_file.fax [question_file.q]\n");
+		if(argc > 3){
+			fprintf(stderr, "Too many arguments.\n");
+		}
+
+		facts = fopen(argv[1], "r");
+		if(facts == NULL){
+			fprintf(stderr, "Error opening facts file: %s\n", argv[1]);
+			exit(1);
+		}
+
+		questions = fopen(argv[2], "r");
+		if(questions == NULL){
+			fprintf(stderr, "Error opening questions file: %s\n", argv[2]);
+			exit(1);
+		}
+
+		process();
 	}
 	
 	return 0;
@@ -44,37 +66,144 @@ int main(int argc, char const *argv[])
 
 void process(void){
 	char c;
+	char *str_name;
+	char *str_prop;
+	char *str_val;
+	int str_size;
+	int str_max;
 
-	size = 0;
+	lst_size = 0;
 	last = 0;
-	root = malloc(sizeof(struct node));
+	root = 0;
 
-	root->next = 0;
-	root->data = 0;
+	c = getc(facts);
+	while(c != EOF){
 
-	while((c = getc(facts)) != EOF){
-		add_node(c);
+		c = trim_white(c, facts);
+		if(c != 'F'){
+			while((c != '\n') && (c != EOF)){
+				// fprintf(stdout, "%c", c);
+				c = getc(facts);
+			}
+		}
+		else{
+			c = getc(facts);
+			c = trim_white(c, facts);
+
+			str_max = 8;
+			str_name = malloc(str_max * sizeof(char));
+			str_size = 0;
+
+			while((c != ':') ){
+				// fprintf(stdout, "%c", c);
+
+				if(str_size >= str_max){
+					str_max += 8;
+					str_name = realloc(str_name, str_max*sizeof(char));
+				}
+				str_name[str_size] = c;
+				str_size++;
+
+				c = getc(facts);
+			}
+			// fprintf(stdout, "\n");
+
+			c = getc(facts);
+			c = trim_white(c, facts);
+
+			str_max = 8;
+			str_prop = malloc(str_max * sizeof(char));
+			str_size = 0;
+
+			while((c != '=') ){
+				// fprintf(stdout, "%c", c);
+
+				if(str_size >= str_max){
+					str_max += 8;
+					str_prop = realloc(str_prop, str_max*sizeof(char));
+				}
+				str_prop[str_size] = c;
+				str_size++;
+
+				c = getc(facts);
+			}
+			// fprintf(stdout, "\n");
+
+			c = getc(facts);
+			c = trim_white(c, facts);
+
+			str_max = 8;
+			str_val = malloc(str_max * sizeof(char));
+			str_size = 0;
+
+			while((c != '\n') ){
+				// fprintf(stdout, "%c", c);
+
+				if(str_size >= str_max){
+					str_max += 8;
+					str_val = realloc(str_val, str_max*sizeof(char));
+				}
+				str_val[str_size] = c;
+				str_size++;
+
+				c = getc(facts);
+			}
+			add_node(str_name, str_prop, str_val);
+		}
+
+		// add_node(c);
+		// fprintf(stdout, "%c", c);
+		c = getc(facts);
 	}
 	print_list();
 	liberate_list();
 }
 
-void add_node(char c){
-	// fprintf(stdout, "add_node(%c)\n", c);
+int add_node(char *p_name, char *p_prop, char *p_val){
+	// fprintf(stdout, "add_node(%s)\n", s);
+	struct node *current;
+	current = root;
 
-	if(last == 0){
-		root->data = c;
+	if(current == 0){
+		current = malloc(sizeof(struct node));
+		current->next = 0;
+		current->name = p_name;
+		current->property = p_prop;
+		current->value = p_val;
+		root = current;
 		last = root;
+		lst_size++;
 	}
 	else{
-		last->next = malloc(sizeof(struct node));
-		last = last->next;
+		while((current->next) != 0){
+			// fprintf(stderr, "strcmp(%s, %s) = %d  &&  strcmp(%s, %s) = %d\n", 
+			// 	current->name, p_name, strcmp(current->name, p_name), 
+			// 	current->property, p_prop, strcmp(current->property, p_prop));
+			
+			if((strcmp(current->name, p_name) == 0) && 
+			(strcmp(current->property, p_prop) == 0) ){
+				current->value = p_val;
+				free(p_name);
+				free(p_prop);
+				free(p_val);
+				fprintf(stderr, "ERORE\n");
+				return 0;
+			}
 
-		last->next = 0;
-		last->data = c;
+			current = current->next;
+		}
+
+		current->next = malloc(sizeof(struct node));
+		current = current->next;
+		current->next = 0;
+		current->name = p_name;
+		current->property = p_prop;
+		current->value = p_val;
+		last = current;
+		lst_size++;
 	}
 
-	size++;
+	return 0;
 }
 
 void print_list(void){
@@ -84,32 +213,40 @@ void print_list(void){
 	current = root;
 
 	if(current != 0){
-		fprintf(stdout, "\nroot");
+		// fprintf(stdout, "\nroot");
 		while(current != 0){
-			fprintf(stdout, " -> %c", current->data);
+			fprintf(stdout, "%s : %s = %s\n", current->name, current->property, current->value);
 			current = current->next;
 		}
-		fprintf(stdout, "<----last\n");
+		// fprintf(stdout, "<----last\n");
 	}
 	else{
-		fprintf(stdout, "Empty List.\n");
+		// fprintf(stdout, "Empty List.\n");
 	}
 }
 
 void liberate_list(void){
-	// fprintf(stdout, "liberate_list()\n");
+	// fprintf(stdout, "LIBERATION! free() the people.\n");
 
 	struct node *temp;
 
 	while(root != 0){
-		fprintf(stdout, "Freeing node: %c\n", root->data);
+		// fprintf(stdout, "Freeing node: %c\n", root->data);
 		temp = root;
 		root = root->next;
+		free(temp->name);
+		free(temp->property);
+		free(temp->value);
 		free(temp);
 	}
 }
 
-
+char trim_white(char c, FILE *input){
+	while((c == ' ') || (c == '\t') || (c == '\n')){
+			c = getc(facts);
+	}
+	return c;
+}
 
 
 
